845dc23eead214838687d01c0461bc0937c6fc90
TwanLee <dthuanle.20@gmail.com>
2022-06-08 23:49:35 +0700

    add toggle hand tracker button

--- lib/game/pages/start/pages/forty_lines/forty_lines_details/forty_lines_details_page.dart
+++ lib/game/pages/start/pages/forty_lines/forty_lines_details/forty_lines_details_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tetris_master/game/core/theme/game_color.dart';
-import 'package:tetris_master/game/core/utils/hand_tracker.dart';
 import 'package:tetris_master/game/core/widgets/tetris_card.dart';
 import 'package:tetris_master/game/data/apis/forty_lines_apis.dart';
 import 'package:tetris_master/game/game_core/controller/audio_controller.dart';
+import 'package:tetris_master/game/game_core/widgets/hand_tracker_widget.dart';
 
 import '../forty_lines_game/forty_lines_game_page.dart';
 
@@ -52,117 +52,94 @@ class FortyLinesDetailPage extends ConsumerWidget {
             }
           }
         },
-        child: ListView(
-          padding: EdgeInsets.only(
-            left: MediaQuery.of(context).size.width * 0.14,
-          ),
+        child: Stack(
           children: [
-            const SizedBox(height: 120),
-            TetrisCard(
-              color: GameColor.brown,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '5 LINES',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 26,
-                      color: GameColor.brown[100],
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    'CLEAR 5 LINES IN THE SHORTEST AMOUNT OF TIME POSSIBLE. SCORE DOESN\'T MATTER HERE. JUST GO FOR THE WORLD RECORD',
-                    style: TextStyle(
-                      color: GameColor.brown[100],
-                      fontSize: 12,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'PERSIONAL BEST',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: GameColor.brown[100],
-                      fontSize: 14,
-                    ),
-                  ),
-                  Text(
-                    fortyLineApis.get().personalBest == null
-                        ? 'N/A'
-                        : Duration(
-                            milliseconds: fortyLineApis.get().personalBest!,
-                          ).toString().substring(0, 9),
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: GameColor.brown[100],
-                      fontSize: 26,
-                    ),
-                  ),
-                ],
+            ListView(
+              padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width * 0.14,
               ),
+              children: [
+                const SizedBox(height: 120),
+                TetrisCard(
+                  color: GameColor.brown,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '5 LINES',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 26,
+                          color: GameColor.brown[100],
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        'CLEAR 5 LINES IN THE SHORTEST AMOUNT OF TIME POSSIBLE. SCORE DOESN\'T MATTER HERE. JUST GO FOR THE WORLD RECORD',
+                        style: TextStyle(
+                          color: GameColor.brown[100],
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'PERSIONAL BEST',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: GameColor.brown[100],
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        fortyLineApis.get().personalBest == null
+                            ? 'N/A'
+                            : Duration(
+                                milliseconds: fortyLineApis.get().personalBest!,
+                              ).toString().substring(0, 9),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: GameColor.brown[100],
+                          fontSize: 26,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    audioController.playClick();
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (_) => const FortyLinesGamePage()),
+                    );
+                  },
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: GameColor.brown[300],
+                      border: Border(
+                        top: BorderSide(color: GameColor.brown[400]!, width: 2),
+                        bottom:
+                            BorderSide(color: GameColor.brown[700]!, width: 2),
+                        left:
+                            BorderSide(color: GameColor.brown[500]!, width: 2),
+                      ),
+                    ),
+                    child: const Text(
+                      'START',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                audioController.playClick();
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => const FortyLinesGamePage()),
-                );
-              },
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                decoration: BoxDecoration(
-                  color: GameColor.brown[300],
-                  border: Border(
-                    top: BorderSide(color: GameColor.brown[400]!, width: 2),
-                    bottom: BorderSide(color: GameColor.brown[700]!, width: 2),
-                    left: BorderSide(color: GameColor.brown[500]!, width: 2),
-                  ),
-                ),
-                child: const Text(
-                  'START',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            GestureDetector(
-              onTap: () async {
-                await HandTracker.start();
-
-                // ignore: use_build_context_synchronously
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => const FortyLinesGamePage()),
-                );
-              },
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                decoration: BoxDecoration(
-                  color: GameColor.brown[300],
-                  border: Border(
-                    top: BorderSide(color: GameColor.brown[400]!, width: 2),
-                    bottom: BorderSide(color: GameColor.brown[700]!, width: 2),
-                    left: BorderSide(color: GameColor.brown[500]!, width: 2),
-                  ),
-                ),
-                child: const Text(
-                  'START WITH HAND TRACKER',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-            ),
+            const HandTrackerWidget(),
           ],
         ),
       ),
--- lib/game/pages/start/start_page.dart
+++ lib/game/pages/start/start_page.dart
@@ -2,7 +2,9 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tetris_master/game/core/theme/game_color.dart';
+import 'package:tetris_master/game/core/utils/hand_tracker.dart';
 import 'package:tetris_master/game/game_core/controller/audio_controller.dart';
+import 'package:tetris_master/game/game_core/widgets/hand_tracker_widget.dart';
 
 import 'pages/forty_lines/forty_lines_details/forty_lines_details_page.dart';
 import 'widgets/tetris_mode_button.dart';
@@ -13,6 +15,7 @@ class StartPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final audioController = ref.watch(audioProvider.notifier);
+
     return Scaffold(
       backgroundColor: Colors.black,
       floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
@@ -46,49 +49,54 @@ class StartPage extends ConsumerWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: MediaQuery.of(context).size.width * 0.14,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TetrisModeButton(
-              color: GameColor.brown,
-              type: '5L',
-              title: '5 LINES',
-              description: 'COMPLETE 10 LINES AS QUICKLY AS POSSIBLE',
-              onTap: () {
-                audioController.playClick();
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const FortyLinesDetailPage(),
-                  ),
-                );
-              },
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.14,
             ),
-            const SizedBox(height: 12),
-            TetrisModeButton(
-              color: GameColor.orange,
-              type: 'BLZ',
-              title: 'BLITZ',
-              description: 'A TWO-MINUTE RACE AGAINST THE CLOCK',
-              onTap: () async {
-                audioController.playClick();
-              },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TetrisModeButton(
+                  color: GameColor.brown,
+                  type: '5L',
+                  title: '5 LINES',
+                  description: 'COMPLETE 10 LINES AS QUICKLY AS POSSIBLE',
+                  onTap: () {
+                    audioController.playClick();
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => const FortyLinesDetailPage(),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 12),
+                TetrisModeButton(
+                  color: GameColor.orange,
+                  type: 'BLZ',
+                  title: 'BLITZ',
+                  description: 'A TWO-MINUTE RACE AGAINST THE CLOCK',
+                  onTap: () async {
+                    audioController.playClick();
+                  },
+                ),
+                const SizedBox(height: 12),
+                TetrisModeButton(
+                  color: GameColor.purple,
+                  type: 'ZEN',
+                  title: 'ZEN',
+                  description: 'RELAX OR TRAIN IN THIS NEVERENDING MODE',
+                  onTap: () {
+                    audioController.playClick();
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 12),
-            TetrisModeButton(
-              color: GameColor.purple,
-              type: 'ZEN',
-              title: 'ZEN',
-              description: 'RELAX OR TRAIN IN THIS NEVERENDING MODE',
-              onTap: () {
-                audioController.playClick();
-              },
-            ),
-          ],
-        ),
+          ),
+          const HandTrackerWidget(),
+        ],
       ),
     );
   }
