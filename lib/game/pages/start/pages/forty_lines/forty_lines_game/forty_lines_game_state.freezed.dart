054ad6ac7b8b215a7219a8e7edfd3c32dccb40ef
TwanLee <dthuanle.20@gmail.com>
2022-06-08 21:34:58 +0700

    gen freezed

---
+++ lib/game/pages/start/pages/forty_lines/forty_lines_game/forty_lines_game_state.freezed.dart
@@ -0,0 +1,448 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'forty_lines_game_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$FortyLinesGameState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() running,
+    required TResult Function(EndGameStatus status, int time) end,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_initial value) initial,
+    required TResult Function(_running value) running,
+    required TResult Function(_end value) end,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $FortyLinesGameStateCopyWith<$Res> {
+  factory $FortyLinesGameStateCopyWith(
+          FortyLinesGameState value, $Res Function(FortyLinesGameState) then) =
+      _$FortyLinesGameStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$FortyLinesGameStateCopyWithImpl<$Res>
+    implements $FortyLinesGameStateCopyWith<$Res> {
+  _$FortyLinesGameStateCopyWithImpl(this._value, this._then);
+
+  final FortyLinesGameState _value;
+  // ignore: unused_field
+  final $Res Function(FortyLinesGameState) _then;
+}
+
+/// @nodoc
+abstract class _$$_initialCopyWith<$Res> {
+  factory _$$_initialCopyWith(
+          _$_initial value, $Res Function(_$_initial) then) =
+      __$$_initialCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_initialCopyWithImpl<$Res>
+    extends _$FortyLinesGameStateCopyWithImpl<$Res>
+    implements _$$_initialCopyWith<$Res> {
+  __$$_initialCopyWithImpl(_$_initial _value, $Res Function(_$_initial) _then)
+      : super(_value, (v) => _then(v as _$_initial));
+
+  @override
+  _$_initial get _value => super._value as _$_initial;
+}
+
+/// @nodoc
+
+class _$_initial implements _initial {
+  _$_initial();
+
+  @override
+  String toString() {
+    return 'FortyLinesGameState.initial()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_initial);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() running,
+    required TResult Function(EndGameStatus status, int time) end,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_initial value) initial,
+    required TResult Function(_running value) running,
+    required TResult Function(_end value) end,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _initial implements FortyLinesGameState {
+  factory _initial() = _$_initial;
+}
+
+/// @nodoc
+abstract class _$$_runningCopyWith<$Res> {
+  factory _$$_runningCopyWith(
+          _$_running value, $Res Function(_$_running) then) =
+      __$$_runningCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_runningCopyWithImpl<$Res>
+    extends _$FortyLinesGameStateCopyWithImpl<$Res>
+    implements _$$_runningCopyWith<$Res> {
+  __$$_runningCopyWithImpl(_$_running _value, $Res Function(_$_running) _then)
+      : super(_value, (v) => _then(v as _$_running));
+
+  @override
+  _$_running get _value => super._value as _$_running;
+}
+
+/// @nodoc
+
+class _$_running implements _running {
+  _$_running();
+
+  @override
+  String toString() {
+    return 'FortyLinesGameState.running()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_running);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() running,
+    required TResult Function(EndGameStatus status, int time) end,
+  }) {
+    return running();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+  }) {
+    return running?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+    required TResult orElse(),
+  }) {
+    if (running != null) {
+      return running();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_initial value) initial,
+    required TResult Function(_running value) running,
+    required TResult Function(_end value) end,
+  }) {
+    return running(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+  }) {
+    return running?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+    required TResult orElse(),
+  }) {
+    if (running != null) {
+      return running(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _running implements FortyLinesGameState {
+  factory _running() = _$_running;
+}
+
+/// @nodoc
+abstract class _$$_endCopyWith<$Res> {
+  factory _$$_endCopyWith(_$_end value, $Res Function(_$_end) then) =
+      __$$_endCopyWithImpl<$Res>;
+  $Res call({EndGameStatus status, int time});
+}
+
+/// @nodoc
+class __$$_endCopyWithImpl<$Res> extends _$FortyLinesGameStateCopyWithImpl<$Res>
+    implements _$$_endCopyWith<$Res> {
+  __$$_endCopyWithImpl(_$_end _value, $Res Function(_$_end) _then)
+      : super(_value, (v) => _then(v as _$_end));
+
+  @override
+  _$_end get _value => super._value as _$_end;
+
+  @override
+  $Res call({
+    Object? status = freezed,
+    Object? time = freezed,
+  }) {
+    return _then(_$_end(
+      status: status == freezed
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as EndGameStatus,
+      time: time == freezed
+          ? _value.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_end implements _end {
+  _$_end({required this.status, required this.time});
+
+  @override
+  final EndGameStatus status;
+  @override
+  final int time;
+
+  @override
+  String toString() {
+    return 'FortyLinesGameState.end(status: $status, time: $time)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_end &&
+            const DeepCollectionEquality().equals(other.status, status) &&
+            const DeepCollectionEquality().equals(other.time, time));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(status),
+      const DeepCollectionEquality().hash(time));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_endCopyWith<_$_end> get copyWith =>
+      __$$_endCopyWithImpl<_$_end>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() running,
+    required TResult Function(EndGameStatus status, int time) end,
+  }) {
+    return end(status, time);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+  }) {
+    return end?.call(status, time);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? running,
+    TResult Function(EndGameStatus status, int time)? end,
+    required TResult orElse(),
+  }) {
+    if (end != null) {
+      return end(status, time);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_initial value) initial,
+    required TResult Function(_running value) running,
+    required TResult Function(_end value) end,
+  }) {
+    return end(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+  }) {
+    return end?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_initial value)? initial,
+    TResult Function(_running value)? running,
+    TResult Function(_end value)? end,
+    required TResult orElse(),
+  }) {
+    if (end != null) {
+      return end(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _end implements FortyLinesGameState {
+  factory _end({required final EndGameStatus status, required final int time}) =
+      _$_end;
+
+  EndGameStatus get status => throw _privateConstructorUsedError;
+  int get time => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  _$$_endCopyWith<_$_end> get copyWith => throw _privateConstructorUsedError;
+}
